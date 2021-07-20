d2401ff2e13370205ab330d8f7459f7bb4523848
Lali Osorio <laliiosorio@gmail.com>
2021-07-20 19:51:27 +0200

    room create db

--- server/routes/room.routes.js
+++ server/routes/room.routes.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
+const User = require('./../models/User.model')
+const Room = require('./../models/Room.model')
 
 
-router.get("/", (req, res, next) => {
-  res.json({ message: 'holi' })
+router.post('/create', (req, res) => {
+
+  const { number, description, bath, price, capacity, type, image } = req.body
+
+
+  Room
+    .create({ number, description, bath, price, capacity, type, image })
+    .then(() => res.json({ code: 200, message: 'Room created' }))
+    .catch(err => res.status(500).json({ code: 500, message: 'DB error while creating Room', err }))
 })
+
+
 
 module.exports = router;
