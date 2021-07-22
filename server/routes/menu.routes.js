044b0798f9d3e9c2520a6f90c1f786987c393f58
Pepe Morcillo <morcillopepe1@gmail.com>
2021-07-22 16:15:09 +0200

    menu endpoints

--- server/routes/menu.routes.js
+++ server/routes/menu.routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const Food = require('./../models/Food.model')
 const MenuPurchase = require('./../models/MenuPurchase.model')
 
+
 //Food-list
 router.get('/foodlist', (req, res) => {
 
@@ -10,80 +11,59 @@ router.get('/foodlist', (req, res) => {
     .then(response => res.json(response))
     .catch(err => console.log(err))
 })
+
+
 //Food-details
 router.get('/food-details/:food_id', (req, res) => {
 
   const { food_id } = req.params
 
   Food
-
     .findById(food_id)
     .then(response => res.json(response))
     .catch(err => console.log(err))
 })
+
+
 //Create-menu
-router.post('/new-menu', (req, res) => {
-  // /:menu_date
-  const user_id = req.session.currentUser._id
-  // const { menu_date } = req.params
-  // const { quantity, dish_id, date } = req.body
-  const { dish_id } = req.query
+router.post('/new/:date', (req, res) => {
+
+  const user = req.session.currentUser._id
+  const { date } = req.params
+  const { dish, quantity } = req.body               //dish will be a checkbox
 
   MenuPurchase
-    .create(user_id, { quantity: 1, dish: dish_id, date: new Date('07/24/2021') })
+    .create({ user, date, dish, quantity })
     .then(response => res.json(response))
     .catch(err => console.log(err))
 })
 
 
+//Delete menu
+router.get('/delete', (req, res) => {
 
+  const { menu_id } = req.query
 
+  MenuPurchase
+    .findByIdAndRemove(menu_id)
+    .then(response => res.json(response))
+    .catch(err => console.log(err))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* router.post('/create', (req, res) => {
-
-  const { description, ingredients, name, vegetarian, image, type } = req.body
-
-
-  Food
-    .create({ description, ingredients, name, vegetarian, image, type })
-    .then(() => res.json({ code: 200, message: 'User created' }))
-    .catch(err => res.status(500).json({ code: 500, message: 'DB error while creating user', err }))
 })
- */
+
+
+//Details-menu
+router.get("/details/:menu_id", (req, res) => {
+
+  const { menu_id } = req.params
+
+  MenuPurchase
+    .findById(menu_id)
+    .populate('dish')
+    .then(response => res.json(response))
+    .catch(err => console.log(err))
+})
+
 
 
 module.exports = router;
